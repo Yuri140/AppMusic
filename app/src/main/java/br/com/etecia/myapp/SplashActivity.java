a7d1767d458e46b5514454be42f853d49798eccb
Yuri140 <yurisousa.carneiro2006@gmail.com>
2024-02-27 21:57:25 -0300

    Splash em teste

--- app/src/main/java/br/com/etecia/myapp/SplashActivity.java
+++ app/src/main/java/br/com/etecia/myapp/SplashActivity.java
@@ -2,9 +2,17 @@ package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
+
+    Intent intent = new Intent(this,MenuPrincipalActivity.class);
+
+    @Override
+    public void startActivity(Intent intent) {
+        super.startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
