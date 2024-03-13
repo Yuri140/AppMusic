162188c6297512b173cfde308912b91035081472
Yuri140 <yurisousa.carneiro2006@gmail.com>
2024-03-12 22:12:06 -0300

    Acho que foi

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -29,6 +29,38 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        lstMusicas = new ArrayList<>();
+
+        lstMusicas.add(new Musicas(R.drawable.gunsnroses, "Vou esuprar vc", 03012024, "Mc Estrupro"));
+//        lstMusicas.add(new Musicas(R.drawable.gunsnroses, "Ficção Científica", " Madhu é abduzida por uma nave intergaláctica";
+//        lstMusicas.add(new Musicas("O sol é para todos","Ficção","2024",R.drawable.gunsnroses));
+//        lstMusicas.add(new Musicas("Eu e Esse Meu Coração", "Auto ajuda", "Eu e Esse Meu Coração", R.drawable.coracao));
+//        lstMusicas.add(new Musicas("Dieta", "Alimentação", "Este não é mais um livro de dieta: O novo e libertador estilo de vida alimentar para saúde e boa", R.drawable.dieta));
+//        lstMusicas.add(new Musicas("O dilema", "Auto ajuda", "O dilema do porco espinho", R.drawable.dilema));
+//        lstMusicas.add(new Musicas("Como fazer Amigos", "Auto ajuda", "Como fazer amigos", R.drawable.amigos));
+//        lstMusicas.add(new Musicas("The Vegitarian ", "Romance", "The Vegitarian Life", R.drawable.thevigitarian));
+//        lstMusicas.add(new Musicas("The Wild Robot", "Categoria do livro", "Descrição do livro", R.drawable.thewildrobot));
+//        lstMusicas.add(new Musicas("The Martian", "Categoria do livro", "Descrição do livro", R.drawable.themartian));
+//        lstMusicas.add(new Musicas("Privacy", "Categoria do livro", "Descrição do livro", R.drawable.privacy));
+//        lstMusicas.add(new Musicas("Maria Semples", "Categoria do livro", "Descrição do livro", R.drawable.mariasemples));
+//        lstMusicas.add(new Musicas("Hedied With", "Categoria do livro", "Descrição do livro", R.drawable.hediedwith));
+//        lstMusicas.add(new Musicas("O dilema", "Auto ajuda", "O dilema do porco espinho", R.drawable.dilema));
+//        lstMusicas.add(new Musicas("Iracema", "Clássico", "Uma das histórias de amor mais aclamadas da literatura brasileira", R.drawable.a_realidade));
+//        lstMusicas.add(new Musicas("O Homem Que Sabia Javanês", "Clássico", "Uma chamada que muitos deixariam passar, Castelo enxergou uma oportunidade única.", R.drawable.ohomequesabia));
+//        lstMusicas.add(new Musicas("Como fazer Amigos", "Auto ajuda", "Como fazer amigos", R.drawable.amigos));
+//        lstMusicas.add(new Musicas("The Vegitarian ", "Romance", "The Vegitarian Life", R.drawable.thevigitarian));
+//        lstMusicas.add(new Musicas("The Wild Robot", "Categoria do livro", "Descrição do livro", R.drawable.thewildrobot));
+//        lstMusicas.add(new Musicas("The Martian", "Categoria do livro", "Descrição do livro", R.drawable.themartian));
+//        lstMusicas.add(new Musicas("Privacy", "Categoria do livro", "Descrição do livro", R.drawable.privacy));
+//        lstMusicas.add(new Musicas("Maria Semples", "Categoria do livro", "Descrição do livro", R.drawable.mariasemples));
+//        lstMusicas.add(new Musicas("Hedied With", "Categoria do livro", "Descrição do livro", R.drawable.hediedwith));
+//        lstMusicas.add(new Musicas("O poder do agora", "Auto Ajuda", "Um guia para a iluminação espiritual ", R.drawable.poder_agora));
+//        lstMusicas.add(new Musicas("Pai rico, pai pobre para jovens", "Administração e Negócios", "o que a escola não ensina sobre dinheiro", "",));
+
+
+
+
+
         recyclerViewMusicas = findViewById(R.id.idRecyclerMusicas);
 
         recyclerViewMusicas.setLayoutManager(new GridLayoutManager(getApplicationContext(),3));
